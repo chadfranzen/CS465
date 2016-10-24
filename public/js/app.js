3f81d95d7e6141d57a11e4d9b4087db77b765283
cdfranz2 <cdfranz2@illinois.edu>
2016-10-24 13:04:29 -0500

    Add bottom bar

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,25 @@
-angular.module('App', ['ngMaterial']);
+var app = angular.module('App', ['ngMaterial', 'ngRoute']);
+
+app.config(function($mdThemingProvider, $routeProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('teal')
+    .accentPalette('blue')
+    .warnPalette('red');
+
+  $routeProvider
+    .when('/discover', {
+      templateUrl: 'partials/discover.html',
+      controller: 'DiscoverController'
+    }).when('/search', {
+      templateUrl: 'partials/search.html',
+      controller: 'SearchController'
+    }).when('/mytours', {
+      templateUrl: 'partials/mytours.html',
+      controller: 'MyToursController'
+    }).when('/create', {
+      templateUrl: 'partials/create.html',
+      controller: 'CreateController'
+    }).otherwise({
+      redirectTo: '/discover'
+    });
+});
---
+++ public/js/controllers.js
@@ -0,0 +1,15 @@
+app.controller('DiscoverController', function($scope) {
+  console.log('DiscoverController running');
+});
+
+app.controller('SearchController', function($scope) {
+  console.log('SearchController running');
+});
+
+app.controller('MyToursController', function($scope) {
+  console.log('MyToursController running');
+});
+
+app.controller('CreateController', function($scope) {
+  console.log('CreateController running');
+});
