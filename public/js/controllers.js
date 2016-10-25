f38a79213ebc30c68f6d422fa84f6b8b4d7a35c2
cdfranz2 <cdfranz2@illinois.edu>
2016-10-25 14:14:17 -0500

    fullscreen map

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,8 +2,15 @@ app.controller('DiscoverController', function($scope) {
   console.log('DiscoverController running');
 });
 
-app.controller('SearchController', function($scope) {
+app.controller('SearchController', function($scope, $timeout, NgMap) {
   console.log('SearchController running');
+
+  // We have to manually resize the map to fit the flex container.
+  NgMap.getMap({id: 'searchmap'}).then(function(map){
+    var height = $('#map-container').height();
+    $('#searchmap').height(height);
+    google.maps.event.trigger(map,'resize');
+  });
 });
 
 app.controller('MyToursController', function($scope) {
