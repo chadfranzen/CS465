eebc8b045f5131804305e4bff3ea87ce80b03422
cdfranz2 <cdfranz2@illinois.edu>
2016-11-05 13:28:13 -0500

    Add color palettes

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,43 @@
 var app = angular.module('App', ['ngRoute', 'ngMap', 'ngMaterial', 'truncate', 'Services', 'Controllers']);
 
 app.config(function($mdThemingProvider, $routeProvider) {
-  $mdThemingProvider.theme('default');
+  var customPurple = $mdThemingProvider.extendPalette('deep-purple', {
+    '500': '#765285', // default primary
+    '600': '#351C4D', // button hover
+    '300': '#765285', // md-hue-1
+    '800': '#351C4D',  // md-hue-2
+    'A100': '#351C4D' // md-hue-3
+  });
+  $mdThemingProvider.definePalette('custom-purple', customPurple);
+
+  var customOrange = $mdThemingProvider.extendPalette('deep-orange', {
+    'A200': '#FEB47B', // default accent
+    'A100': '#F5AB99', // md-hue-1
+    'A400': '#FF7E5F', // md-hue-2
+    'A700': '#FF7E5F' // md-hue-3
+  });
+  $mdThemingProvider.definePalette('custom-orange', customOrange);
+
+  var customBlue = $mdThemingProvider.extendPalette('cyan', {
+    '500': '#8BCBC8', // default primary
+    '600': '#DAE9E4', // button hover
+    '300': '#DAE9E4', // md-hue-1
+    '800': '#8BCBC8',  // md-hue-2
+    'A100': '#3C2E3D' // md-hue-3
+  });
+  $mdThemingProvider.definePalette('custom-blue', customBlue);
+
+  var customPink = $mdThemingProvider.extendPalette('deep-orange', {
+    'A200': '#ECC7C0', // default accent
+    'A100': '#ECC7C0', // md-hue-1
+    'A400': '#ECC7C0', // md-hue-2
+    'A700': '#FDAE84' // md-hue-3
+  });
+  $mdThemingProvider.definePalette('custom-pink', customPink);
+
+  $mdThemingProvider.theme('default')
+    .primaryPalette('cyan')
+    .accentPalette('pink');
   $mdThemingProvider.enableBrowserColor();
 
   $routeProvider
